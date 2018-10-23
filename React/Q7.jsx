323ea334fa5c633499d935d2b5de9c2bd1a1786f
msfarookhy <msfarookhy@gmail.com>
2018-10-23 20:30:09 +0530

    Create a Stateless Functional Component

---
+++ React/Q7.jsx
@@ -0,0 +1,10 @@
+const MyComponent = function() {
+    // change code below this line
+  return (
+    <div> {MyComponent}this is first coment
+    </div>
+  )
+  
+  
+    // change code above this line
+  }
