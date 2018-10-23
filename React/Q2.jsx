6763b6a6094e848646a7cc4d6328ddf08f821e5d
msfarookhy <msfarookhy@gmail.com>
2018-10-23 17:21:17 +0530

    Create a Complex JSX Element

---
+++ React/Q2.jsx
@@ -0,0 +1,12 @@
+// writcoe your code here
+const JSX=
+<div>
+<h1>Hello JSX</h1>
+<p> PAragraph </p>
+<ul>
+<li> this is jsx</li>
+<li> hello jsx</li>
+<li> third line of jsx</li>
+
+</ul>
+ </div>
